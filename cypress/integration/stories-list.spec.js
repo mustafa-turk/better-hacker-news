a19ae00f2e5a9a77bfe3ff9e67f5648b70f7ab89
Mustafa Turk <mtafa.turk@gmail.com>
2022-10-01 19:05:51 +0200

    fix e2e tests

--- cypress/integration/stories-list.spec.js
+++ cypress/integration/stories-list.spec.js
@@ -3,6 +3,5 @@ describe('stories list', () => {
     cy.visit('/');
 
     cy.get('[data-testid=title]').should('contain', 'Some title');
-    cy.get('[data-testid=comments-count]').should('contain', '3 comments');
   });
 });
--- mocks/handlers.js
+++ mocks/handlers.js
@@ -1,23 +1,24 @@
 import { rest } from 'msw';
 import { STORY_DETAILS_URL, TOP_STORIES_URL } from 'helpers/constants';
 
-export const handlers = [
-  rest.get(TOP_STORIES_URL, (_req, res, ctx) => {
-    return res(ctx.json(['1']));
-  }),
-  rest.get(`${STORY_DETAILS_URL}/1.json`, (_req, res, ctx) => {
-    return res(
-      ctx.json({
-        by: 'bmlw',
-        descendants: 3,
-        id: 1,
-        kids: [2, 3, 4],
-        score: 123,
-        time: 1659296371,
-        title: 'Some title',
-        type: 'story',
-        url: 'http://some-url.com',
-      }),
-    );
-  }),
-];
+const topStoriesMock = rest.get(TOP_STORIES_URL, (_req, res, ctx) => {
+  return res(ctx.json(['1']));
+});
+
+const storyDetailsMock = rest.get(`${STORY_DETAILS_URL}/1.json`, (_req, res, ctx) => {
+  return res(
+    ctx.json({
+      by: 'bmlw',
+      descendants: 3,
+      id: 1,
+      kids: [2, 3, 4],
+      score: 123,
+      time: 1659296371,
+      title: 'Some title',
+      type: 'story',
+      url: 'http://some-url.com',
+    }),
+  );
+});
+
+export const handlers = [topStoriesMock, storyDetailsMock];
